3497624595ac705bd383a841d85fb6521845f508
wkolowski <wkolowski@protonmail.com>
2026-02-14 23:57:43 +0100

    First attempt at using jsCoq.

--- scripts/build_coqdoc.sh
+++ scripts/build_coqdoc.sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 
+# Create a directory in which .v files will be post-processed
+# for the purpose of making them jscoq-friendly.
+cp -r src/ postprocessed/
+
+#sed -E -i 's/\(\* begin hide \*\)\nProof\.\n(.|\n)*?\n\(\* end hide \*\)/Proof.\nAdmitted./g' $(find postprocessed -name "*.v")
+
+#sed -E -i 's/\(\* ===>(.|\n)*?\*\)\n//g' $(find postprocessed -name "*.v")
+
+# Regexes made manually in gedit, adapted to sed by Claude Sonnet 4.5
+find postprocessed -name "*.v" -exec sed -i -E ':a;N;$!ba;s/\(\* begin hide \*\)\nProof\.\n([^\*]|\*[^)])*\n\(\* end hide \*\)/Proof.\nAdmitted./g' {} +
+
+find postprocessed -name "*.v" -exec sed -i -E ':a;N;$!ba;s/\(\* ===>[^*]*(\*[^)][^*]*)*\*\)\n//g' {} +
+
 # Build a HTML page from the Coq comments using coqdoc.
-coqdoc src/*v                                        \
+coqdoc postprocessed/*v                              \
   -d docs                                            \
   --html                                             \
-  --with-header assets/header.html                   \
   --with-footer assets/footer.html                   \
   --no-lib-name                                      \
   --lib-subtitles                                    \
   --parse-comments                                   \
   --no-index
 
-# Move the necessary assets to docs/ and rename the generated page.
-cp assets/* docs/
+cp assets/coqdoc.css docs/
 mv docs/Seminar.html docs/index.html
+
+rm -rf postprocessed/
+
